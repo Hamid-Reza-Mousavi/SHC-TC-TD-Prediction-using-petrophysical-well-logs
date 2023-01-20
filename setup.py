fabefe0f57cdafe5f2d10ff1f9be9dab6530bda6
Hamid Reza Mousavi <97799794+Esfahani98@users.noreply.github.com>
2023-01-20 23:11:22 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
         name="SHC|TC|TD Prediction",
-        version="1.0.0",
+        version="1.1.0",
         url="https://github.com/Esfahani98/SHC-TC-TD-Prediction.git",
-        author="Hamid reza mousavi",
+        author="Hamid Reza Mousavi",
         license="IUST",
         packages=["Models"],
         include_package_data=True,
